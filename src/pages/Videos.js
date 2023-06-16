da368095b78f818360f164a41153705b87d59228
joaovictorff <136546511+joaovictorff@users.noreply.github.com>
2023-06-16 18:00:30 -0300

    Add files via upload

---
+++ src/pages/Videos.js
@@ -0,0 +1,34 @@
+import React, { useRef, useState } from "react";
+import VideoFooter from "./components/footer/VideoFooter";
+import "./video.css";
+
+function Videos() {
+  const videoRef = useRef(null);
+  const [play, setPlay] = useState(false);
+
+  function handdleStart() {
+    if (play) {
+      videoRef.current.pause();
+      setPlay(false);
+    } else {
+      videoRef.current.play();
+      setPlay(true);
+    }
+  }
+
+  return (
+    <div className="video">
+      <video
+        className="video__player"
+        ref={videoRef}
+        onClick={handdleStart}
+        loop
+        src="https://firebasestorage.googleapis.com/v0/b/jornada-dev.appspot.com/o/brecker2.mp4?alt=media&token=b5399418-9276-4e53-a706-1e00290c2c74"
+      ></video>
+      {/* Side bar */}
+      <VideoFooter />      
+    </div>
+  );
+}
+
+export default Videos;
